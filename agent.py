0b8ff3e9c38c51f7815799999860c8762b31a42b
Stampe04 <s244677@dtu.dk>
2025-01-10 11:00:50 +0100

    comments in agent

--- agent.py
+++ agent.py
@@ -6,8 +6,10 @@ import random
 import numpy as np
 from collections import deque
 
+# amount of neurons in each hidden layer
 H = 250
 
+# the neural network
 class Model(torch.nn.Module):
     def __init__(self, input_dim, output_dim, lr=0.01):
         super(Model, self).__init__()
@@ -15,27 +17,32 @@ class Model(torch.nn.Module):
         self.fc2 = nn.Linear(H, H)
         self.fc3 = nn.Linear(H, output_dim)
 
+        # Uses Adam for optimization
         self.optimizer = torch.optim.Adam(self.parameters(), lr=lr)
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.loss_function = nn.MSELoss() # if not working add reduction='sum'
         self.to(self.device)
     
+    # Uses ReLU activation function to output raw Q-values
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
+# the agent
 class Agent():
     theTerminalState = False
     
     def __init__(self, id, gamma, epsilon, lr, input_dim, output_dim, samplesize, epsilon_decay=5e-4, epsilon_min=0.01, batchMaxLength=100_000):
         self.id = id
 
+        # creates replay memory
         self.batchMaxLength = batchMaxLength
         self.batch = deque(maxlen = self.batchMaxLength)
         self.replay_buffer = ReplayBuffer(self.batchMaxLength)
 
+        # defines parameters
         self.gamma = gamma
         self.epsilon = epsilon
         self.epsilon_decay = epsilon_decay
@@ -44,16 +51,21 @@ class Agent():
         self.samplesize = samplesize
         self.index = 0
 
+        # initializes the neural network
         self.model = Model(input_dim, output_dim, lr=lr)
         self.weightPath = f"Models/{self.id}.pt"
 
+        # tracks the agents current state, action, rewards, next state and terminal
         self.state_mem = np.zeros((self.batchMaxLength, *input_dim), dtype=np.float32)
         self.new_state_mem = np.zeros((self.batchMaxLength, *input_dim), dtype=np.float32)
         self.action_mem = np.zeros(self.batchMaxLength, dtype=np.int32)
         self.reward_mem = np.zeros(self.batchMaxLength, dtype=np.float32)
         self.terminal_mem = np.zeros(self.batchMaxLength, dtype=np.bool)
     
+    # selects an action from the current state
     def act(self, obs):    
+
+        # decides wether to prioritize exploration or exploitation
         if random.uniform(0, 1) < self.epsilon:
             return random.randint(0, 39)
         else:
@@ -61,7 +73,7 @@ class Agent():
             actions = self.model.forward(state)
             return torch.argmax(actions).item()
     
-    # this stores the experience in the batch
+    # stores the experience in the batch
     def updateBatch(self, state, action, reward, new_state, done):
         i = self.index % self.batchMaxLength
         self.state_mem[i] = state
@@ -74,6 +86,8 @@ class Agent():
     
     # learns from the experience
     def experience(self):
+
+        # checks if there are enough experiences in the memory
         if self.index < self.samplesize:
             return
 
@@ -82,11 +96,13 @@ class Agent():
         
         self.model.optimizer.zero_grad()
         
+        # samples randomly 
         max_size = min(self.batchMaxLength, self.index)
         batch = np.random.choice(max_size, self.samplesize, replace=False)
 
         batch_index = np.arange(self.samplesize, dtype=np.int32)
 
+        # converts data to tensors
         state_batch = torch.tensor(self.state_mem[batch]).to(self.model.device)
 
         action_batch = self.action_mem[batch]
@@ -95,14 +111,21 @@ class Agent():
         reward_batch = torch.tensor(self.state_mem[batch]).to(self.model.device)
         terminal_batch = torch.tensor(self.state_mem[batch]).to(self.model.device)
 
+        # calculates target Q-values
         q_values = self.model.forward(state_batch)[batch_index, action_batch]
         q_values_next = self.model.forward(new_state_batch)
         q_values_next[terminal_batch] = 0.0
 
+        # calculates predicted Q-values
         q_reward = reward_batch + self.gamma * torch.max(q_values_next, dim=1)[0]
 
+        # computes loss
         loss_funtion = self.model.loss_function(q_reward, q_values).to(self.model.device)
+        
+        # backpropagates loss
         loss_funtion.backward()
+        
+        # updates network weights using the optimizer
         self.model.optimizer.step()
 
     # this loads the weights of the model
