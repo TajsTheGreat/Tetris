296853235cc2ea9792ad01ae62fea9e23eba367f
TheisTheGreat <theishove@gmail.com>
2025-01-22 10:03:40 +0100

    forhåbentlig ikke ødelæggende

--- makeStats.py
+++ makeStats.py
@@ -8,32 +8,33 @@ with open("Models/PeterDQNFINALDATA_experiment_data.csv") as f:
 with open("Models/PeterDDQNFINALDATA_experiment_data.csv") as f:
     DDQN = pd.read_csv(f)
 
-with open("Models/PeterDUELINGDQNFINALDATA_experiment_data.csv") as f:
+with open("Models/PeterDUELINGDQNFINALDATA2_experiment_data.csv") as f:
     DUELINGDQN = pd.read_csv(f)
 
-with open("Models/DDDQNtest2_experiment_data.csv") as f:
+with open("Models/DDDQNtest3_experiment_data.csv") as f:
     DDDQN = pd.read_csv(f)
 
-output_file("Tetris/Models/Stats.html")
+output_file("Models/Stats.html")
 
-print(DQN.columns)
-
-p = figure(title="Tetris Dueling DDQN vs DDQN vs DQN vs Dueling DQN", x_axis_label='per 2500', y_axis_label='Average number of positive games')
+p = figure(title="Average number of positive games for every 2500 moves", x_axis_label='Datapoint for every 2500 moves', y_axis_label='Precentage of positive games', height=400)
 p.line(DQN['x'], DQN['num_pos_games'], line_width=1.5, legend_label="DQN", color="blue")
 p.line(DDQN['x'], DDQN['num_pos_games'], line_width=1.5, legend_label="DDQN", color="red")
 p.line(DUELINGDQN['x'], DUELINGDQN['num_pos_games'], line_width=1.5, legend_label="Dueling DQN", color="green")
 p.line(DDDQN['x'], DDDQN['num_pos_games'], line_width=1.5, legend_label="Dueling DDQN", color="orange")
+p.legend.location = "bottom_right"
 
-p2 = figure(title="Tetris Dueling DDQN vs DDQN vs DQN vs Dueling DQN", x_axis_label='per 2500', y_axis_label='Average Moves')
+p2 = figure(title="Average number of moves per game for every 2500 games", x_axis_label='Datapoint for every 2500 moves', y_axis_label='Number of moves on average', height=400)
 p2.line(DQN['x'], DQN['avg_moves'], line_width=1.5, legend_label="DQN", color="blue")
 p2.line(DDQN['x'], DDQN['avg_moves'], line_width=1.5, legend_label="DDQN", color="red")
 p2.line(DUELINGDQN['x'], DUELINGDQN['avg_moves'], line_width=1.5, legend_label="Dueling DQN", color="green")
 p2.line(DDDQN['x'], DDDQN['avg_moves'], line_width=1.5, legend_label="Dueling DDQN", color="orange")
+p2.legend.location = "top_left"
 
-p3 = figure(title="Tetris Dueling DDQN vs DDQN vs DQN vs Dueling DQN", x_axis_label='per 2500', y_axis_label='Average Reward')
+p3 = figure(title="Average reward per move for every 2500 moves", x_axis_label='Datapoint for every 2500 moves', y_axis_label='Average reward per move', height=400)
 p3.line(DQN['x'], DQN['reward'], line_width=1.5, legend_label="DQN", color="blue")
 p3.line(DDQN['x'], DDQN['reward'], line_width=1.5, legend_label="DDQN", color="red")
 p3.line(DUELINGDQN['x'], DUELINGDQN['reward'], line_width=1.5, legend_label="Dueling DQN", color="green")
 p3.line(DDDQN['x'], DDDQN['reward'], line_width=1.5, legend_label="Dueling DDQN", color="orange")
+p3.legend.location = "top_left"
 
 save(row(p, p2, p3))
